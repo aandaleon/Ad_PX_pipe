cb33c6383de4cdf222d3a6b04eb7e94f68369eaf
Angela Andaleon <angie.andaleon@gmail.com>
2018-12-07 09:43:38 -0600

    option to choose gemma path -gp

--- 05c_GEMMA_loop.sh
+++ 05c_GEMMA_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GEMMAPath=/usr/local/bin
+GEMMAPath=/usr/local/bin/gemma
 hackedDefault=False
 hacked=False
 while :
@@ -25,14 +25,18 @@ do
       	 	CovFile="$2"
 	        shift 2
 	        ;;
-	  -o) #output prefix
+      -o) #output prefix
       	 	Prefix="$2"
 	        shift 2
 	        ;;
-	  -h) #switch to "hacked" mode
-			hacked=True
-			shift 1
-			;;
+      -h) #switch to "hacked" mode
+		hacked=True
+		shift 1
+		;;
+      -gp) #path to GEMMA
+		GEMMAPath="$2"
+		shift 2
+		;;
       -*) #unknown 
       		echo "Error: Unknown option: $1" >&2
 	        exit 1
@@ -49,15 +53,15 @@ case "${hacked:=$hackedDefault}" in
 		declare -a tissues=("AFA" "AFHI" "ALL" "CAU" "HIS" "Adipose_Subcutaneous" "Adipose_Visceral_Omentum" "Adrenal_Gland" "Artery_Aorta" "Artery_Coronary" "Artery_Tibial" "Brain_Anterior_cingulate_cortex_BA24" "Brain_Caudate_basal_ganglia" "Brain_Cerebellar_Hemisphere" "Brain_Cortex" "Brain_Frontal_Cortex_BA9" "Brain_Hippocampus" "Brain_Hypothalamus" "Brain_Nucleus_accumbens_basal_ganglia" "Brain_Putamen_basal_ganglia" "Breast_Mammary_Tissue" "Cells_EBV-transformed_lymphocytes" "Cells_Transformed_fibroblasts" "Colon_Sigmoid" "Colon_Transverse" "Esophagus_Gastroesophageal_Junction" "Esophagus_Mucosa" "Esophagus_Muscularis" "Heart_Atrial_Appendage" "Heart_Left_Ventricle" "Liver" "Lung" "Muscle_Skeletal" "Nerve_Tibial" "Ovary" "Pancreas" "Pituitary" "Prostate" "Skin_Not_Sun_Exposed_Suprapubic" "Skin_Sun_Exposed_Lower_leg" "Small_Intestine_Terminal_Ileum" "Spleen" "Stomach" "Testis" "Thyroid" "Uterus" "Vagina" "Whole_Blood") 
 		for tissue in "${tissues[@]}";
 		do
-			echo "${GEMMAPath}"/gemma -g "${GenoFile}""${tissue}".txt -p "${PhenoFile}" -k "${RelFile}" -c "${CovFile}" -lmm 4 -notsnp -o "${Prefix}""${tissue}"
-			"${GEMMAPath}"/gemma -g "${GenoFile}""${tissue}".txt -p "${PhenoFile}" -k "${RelFile}" -c "${CovFile}" -lmm 4 -notsnp -o "${Prefix}""${tissue}"
+			echo "${GEMMAPath}" -g "${GenoFile}""${tissue}".txt -p "${PhenoFile}" -k "${RelFile}" -c "${CovFile}" -lmm 4 -notsnp -o "${Prefix}""${tissue}"
+			"${GEMMAPath}" -g "${GenoFile}""${tissue}".txt -p "${PhenoFile}" -k "${RelFile}" -c "${CovFile}" -lmm 4 -notsnp -o "${Prefix}""${tissue}"
 		done
 		;;
 	False) #for regular GEMMA
 		for chr in {1..22};
 		do
-			echo "${GEMMAPath}"/gemma -g "${GenoFile}""${chr}".txt.gz -p "${PhenoFile}" -a "${AnnoFile}" -k "${RelFile}" -c "${CovFile}" -lmm 4 -o "${Prefix}"chr"${chr}"
-			"${GEMMAPath}"/gemma -g "${GenoFile}""${chr}".txt.gz -p "${PhenoFile}" -a "${AnnoFile}""${chr}".txt -k "${RelFile}" -c "${CovFile}" -lmm 4 -o "${Prefix}"chr"${chr}"
+			echo "${GEMMAPath}" -g "${GenoFile}""${chr}".txt.gz -p "${PhenoFile}" -a "${AnnoFile}" -k "${RelFile}" -c "${CovFile}" -lmm 4 -o "${Prefix}"chr"${chr}"
+			"${GEMMAPath}" -g "${GenoFile}""${chr}".txt.gz -p "${PhenoFile}" -a "${AnnoFile}""${chr}".txt -k "${RelFile}" -c "${CovFile}" -lmm 4 -o "${Prefix}"chr"${chr}"
 		done
 		;;
 esac
