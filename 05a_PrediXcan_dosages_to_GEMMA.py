b99027ee92424cbe886d30fe7bfa23b5f9e27993
Angela Andaleon <angie.andaleon@gmail.com>
2018-12-20 14:04:13 -0600

    Update 05a_PrediXcan_dosages_to_GEMMA.py

--- 05a_PrediXcan_dosages_to_GEMMA.py
+++ 05a_PrediXcan_dosages_to_GEMMA.py
@@ -14,13 +14,14 @@ args = parser.parse_args()
 print("Reading input files.")
 dosage_path = args.dosage_path
 dosage_suffix = args.dosage_suffix
-BIMBAM_path = "BIMBAM/"
+BIMBAM_path = "BIMBAM/" #this was going to be a user option but I didn't see a point
 anno_path = "anno/"
 
+#make BIMBAM and anno folders if they don't already exist
 os.system("mkdir -p BIMBAM")
 os.system("mkdir -p anno")
 
-if args.chr is None: #if no argument given to run a certain chr, run all chr
+if args.chr is None: #if no argument given to run a certain chr, run all chr (I don't think this flag works... might remove)
     chrs_to_test = range(1, 23)
 if args.chr is not None:
     chrs_to_test = range(int(args.chr), (int(args.chr) + 1))
@@ -29,23 +30,23 @@ if args.chr is not None:
 dosage_path = "dosages/"
 '''
 
-dosage_samples = pd.read_csv(dosage_path + "samples.txt", delim_whitespace = True, header = None)
+dosage_samples = pd.read_csv(dosage_path + "samples.txt", delim_whitespace = True, header = None) #read in samples.txt file from PrediXcan-style dosages
 dosage_samples = dosage_samples[[1]] #take only IIDs
 
 print("Starting conversion from PLINK dosage to GEMMA input BIMBAM and anno.")
-for i in chrs_to_test:
-    anno = open(anno_path + "anno" + str(i) + ".txt", "w")
-    BIMBAM = gzip.open(BIMBAM_path + "chr" + str(i) + ".txt.gz", "wb")
-    for line in gzip.open(dosage_path + "chr" + str(i) + dosage_suffix, "rb"):
-        arr = line.strip().split()
-        (chr, rs, bp, A1, A2, MAF) = arr[0:6]
-        if len(A1) < 2 and len(A2) < 2:
-           dosages = arr[6:]
-           dosages = [str(dosage) for dosage in dosages]
-           dosages_str = '\t'.join(dosages)
-           BIMBAM_format = (rs + "\t" + A1 + "\t" + A2 + "\t" + dosages_str + "\n")
+for i in chrs_to_test: #for each chr
+    anno = open(anno_path + "anno" + str(i) + ".txt", "w") #start an annotation file
+    BIMBAM = gzip.open(BIMBAM_path + "chr" + str(i) + ".txt.gz", "wb") #start a genotype file
+    for line in gzip.open(dosage_path + "chr" + str(i) + dosage_suffix, "rb"): #open PrediXcan-style dosage file
+        arr = line.strip().split() #split each line of PrediXcan-style dosage file into an array
+        (chr, rs, bp, A1, A2, MAF) = arr[0:6] #extract non-genotype information into tuple
+        if len(A1) < 2 and len(A2) < 2: #keep only alleles, no indels
+           dosages = arr[6:] #pull out dosages into array
+           dosages = [str(dosage) for dosage in dosages] #convert dosages all into str
+           dosages_str = '\t'.join(dosages) #separate dosages by delimiter and convert to str
+           BIMBAM_format = (rs + "\t" + A1 + "\t" + A2 + "\t" + dosages_str + "\n") #write genotype to BIMBAM file in proper format
            BIMBAM.write(BIMBAM_format)
-           anno.write(rs + "\t" + bp + "\t" + str(i) + "\n")
+           anno.write(rs + "\t" + bp + "\t" + str(i) + "\n") #write annotations to anno file in proper format
     BIMBAM.close()
     anno.close()
     print("Completed with chr " + str(i) + ".")
