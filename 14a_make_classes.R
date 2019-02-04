28a38e28b9ccdd03e7b0faf1c3bd0d2bf7600796
Angela Andaleon <angie.andaleon@gmail.com>
2019-02-04 16:23:17 -0600

    notes and make more universal

--- 14a_make_classes.R
+++ 14a_make_classes.R
@@ -2,7 +2,7 @@
 "%&%" = function(a,b) paste(a,b,sep="")
 library(dplyr)
 library(data.table)
-args <- commandArgs(trailingOnly=T)
+args <- commandArgs(trailingOnly = T)
 phind_file_name <- args[1] #name of .phind file that HAPI-UR outputted
 pop_file_name <- args[2] #name of pop file with three columns: FID, IID, and pop
 test_pop <- args[3] #code of population to be tested
@@ -11,15 +11,15 @@ test_pop <- args[3] #code of population to be tested
 phind <- fread(phind_file_name, header = F)
 phind$V2 <- NULL
 phind$V3 <- NULL
-phind$FID <- gsub("\\:.*", "", phind$V1)
+phind$FID <- gsub("\\:.*", "", phind$V1) #separate FID and IID columns
 phind$IID <- gsub(".*:\\s*|_.*", "", phind$V1)
 phind$V1 <- NULL
 
 #add pops
-pop_file <- fread(pop_file_name, header = F)
+pop_file <- fread("/home/angela/Ad_PX_pipe_data/RFMix/RefPop/" %&% pop_file_name %&% "_pop.txt", header = F)
 colnames(pop_file) <- c("FID", "IID", "pop")
 ordered_phind <- left_join(phind, pop_file, by = c("FID", "IID"))
-ordered_phind$pop <- as.numeric(factor(ordered_phind$pop, exclude = test_pop))
-ordered_phind$pop[is.na(ordered_phind$pop)] <- "0"
+ordered_phind$pop <- as.numeric(factor(ordered_phind$pop, exclude = test_pop)) #convert pops to ints
+ordered_phind$pop[ordered_phind$pop == max(ordered_phind$pop)] <- "0" #assign 0 to test pop (which should be the max if ordered properly)
 write(paste(as.character(ordered_phind$pop), collapse = " "), test_pop %&% ".classes")
 
