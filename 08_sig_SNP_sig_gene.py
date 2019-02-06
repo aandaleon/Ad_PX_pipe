b8982931896c5411eddf242a2a43d194db346b39
Angela Andaleon <angie.andaleon@gmail.com>
2019-02-06 13:15:39 -0600

    remove input prefix

--- 08_sig_SNP_sig_gene.py
+++ 08_sig_SNP_sig_gene.py
@@ -8,13 +8,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--desc", type = str, action = "store", dest = "desc", required = False, help = "Finds significant SNPs and genes from GEMMA output.")
 parser.add_argument("--SNP_sig", type = str, action = "store", dest = "SNP_sig", required = False, default = "5e-8", help = "Significance threshold for SNPs. Default = 5e-8")
 parser.add_argument("--gene_sig", type = str, action = "store", dest = "gene_sig", required = False, default = "9.654e-6", help = "Significance threshold for genes. Default = 9.654-6")
-parser.add_argument("--input_prefix", type = str, action = "store", dest = "input_prefix", required = True, help = "Prefix for input, not including 'output/'.")
+#parser.add_argument("--input_prefix", type = str, action = "store", dest = "input_prefix", required = True, help = "Prefix for input, not including 'output/'.")
 parser.add_argument("--pheno_names", type = str, action = "store", dest = "pheno_names", required = False, default = "pheno_names.txt", help = "File containing pheno names. Default = 'pheno_names.txt'.")
 args = parser.parse_args()
 
 SNP_sig = float(args.SNP_sig)
 gene_sig = float(args.gene_sig)
-input_prefix = args.input_prefix
+#input_prefix = args.input_prefix
+input_prefix = ""
 pheno_names = list(np.loadtxt(args.pheno_names, dtype = str))
 
 '''
@@ -22,15 +23,14 @@ SNP_sig = 5e-8
 gene_sig = 9.654e-6
 input_prefix = "AMR"
 '''
-
-sig_snp = open("output/" + input_prefix + "_sig_snps.txt", "w")
-sig_snp.write("chr\trs\tps\tn_miss\tallele1\tallele0\taf\tbeta\tse\tl_remle\tl_mle\tp_wald\tp_lrt\tp_score\tpheno\n")
-sig_gene = open("output/" + input_prefix + "_sig_genes.txt", "w")
-sig_gene.write("chr\trs\tps\tn_miss\tallele1\tallele0\taf\tbeta\tse\tl_remle\tl_mle\tp_wald\tp_lrt\tp_score\ttissue\tpheno\n")
-        
+ 
 for pheno_name in pheno_names:
+    sig_snp = open("output/" + pheno_name + "_sig_snps.txt", "w")
+    sig_snp.write("chr\trs\tps\tn_miss\tallele1\tallele0\taf\tbeta\tse\tl_remle\tl_mle\tp_wald\tp_lrt\tp_score\tpheno\n")
+    sig_gene = open("output/" + pheno_name + "_sig_genes.txt", "w")
+    sig_gene.write("chr\trs\tps\tn_miss\tallele1\tallele0\taf\tbeta\tse\tl_remle\tl_mle\tp_wald\tp_lrt\tp_score\ttissue\tpheno\n")
     for chrom in range(1, 23): #find sig snp
-        for line in open("output/" + input_prefix + "_" + pheno_name + "_chr" + str(chrom) + ".assoc.txt", "r"):
+        for line in open("output/" + input_prefix + "" + pheno_name + "_chr" + str(chrom) + ".assoc.txt", "r"):
             arr = line.strip().split()
             (chr, rs, ps, n_miss, allele1, allele0, af, beta, se, l_remle, l_mle, p_wald, p_lrt, p_score) = arr[0:14]
             if line.startswith("chr"): #the header line messes things up so just skip it
@@ -40,7 +40,7 @@ for pheno_name in pheno_names:
                 sig_snp.write(is_sig_snp)
 
     for tissue in tissues: #find sig gene
-        for line in open("output/" + input_prefix + "_" + pheno_name + "_" + tissue + ".assoc.txt", "r"):
+        for line in open("output/" + input_prefix + "" + pheno_name + "_" + tissue + ".assoc.txt", "r"):
             arr = line.strip().split()
             (chr, rs, ps, n_miss, allele1, allele0, af, beta, se, l_remle, l_mle, p_wald, p_lrt, p_score) = arr[0:14]
             if line.startswith("chr"):
@@ -51,4 +51,4 @@ for pheno_name in pheno_names:
                 
 sig_snp.close()
 sig_gene.close()
-print("Completed writing output/" + input_prefix + "_sig_snps.txt and output/" + input_prefix + "_sig_genes.txt")
+print("Completed writing sig_SNPs.txt and sig_gene.txt for all phenotypes.")
