1c21a12bf444ea2bfe667b61f63d65fc84ed45de
Angela Andaleon <angie.andaleon@gmail.com>
2019-02-06 13:20:12 -0600

    remove prefix

--- 10b_COLOC_wrapper.py
+++ 10b_COLOC_wrapper.py
@@ -11,7 +11,7 @@ tissues_sample_size= ["233", "578", "352", "585", "1163", "298", "298", "126", "
 
 parser = argparse.ArgumentParser() #I could do this in bash but flag passing in bash is terrible
 parser.add_argument("--desc", type = str, action = "store", dest = "desc", required = False, help = "make GWAS output into GCTA-COJO format")
-parser.add_argument("--prefix", type = str, action = "store", dest = "prefix", required = True, help = "Prefix of input files (not including 'COLOC_input/').")
+#parser.add_argument("--prefix", type = str, action = "store", dest = "prefix", required = True, help = "Prefix of input files (not including 'COLOC_input/').")
 parser.add_argument("--GWAS_sample_size", type = str, action = "store", dest = "GWAS_sample_size", required = True, help = "Sample size of GWAS.")
 parser.add_argument("--pheno_names", type = str, action = "store", dest = "pheno_names", required = False, default = "pheno_names.txt", help = "File containing pheno names. Default = 'pheno_names.txt'.")
 args = parser.parse_args()
@@ -19,7 +19,7 @@ args = parser.parse_args()
 os.system("if cd summary-gwas-imputation; then git pull; else git clone https://github.com/hakyimlab/summary-gwas-imputation.git; fi") #contains COLOC wrapper, https://stackoverflow.com/questions/15602059/git-shortcut-to-pull-with-clone-if-no-local-there-yet
 os.system("mkdir -p COLOC_results/")
 
-prefix = args.prefix
+#prefix = args.prefix
 GWAS_sample_size = args.GWAS_sample_size
 pheno_names = list(np.loadtxt(args.pheno_names, dtype = str))
 
@@ -32,7 +32,7 @@ pheno_names = list(np.loadtxt("pheno_names.txt", dtype = str))
 print("Leave this script running overnight as well.")
 for pheno_name in pheno_names:
     for tissue, tissue_sample_size in zip(tissues, tissues_sample_size):
-        run_COLOC = "python3 summary-gwas-imputation/src/run_coloc.py -gwas COLOC_input/" + prefix + "_" + pheno_name + "_GWAS_" + tissue + ".txt.gz -gwas_sample_size " + GWAS_sample_size + " -eqtl COLOC_input/" + prefix + "_" + pheno_name + "_eQTL_" + tissue + ".txt.gz -eqtl_sample_size " + tissue_sample_size + " -output COLOC_results/" + prefix + "_" + pheno_name + "_" + tissue + ".txt.gz" #COLOC prints out too much so throw it into a file
+        run_COLOC = "python3 summary-gwas-imputation/src/run_coloc.py -gwas COLOC_input/" + pheno_name + "_GWAS_" + tissue + ".txt.gz -gwas_sample_size " + GWAS_sample_size + " -eqtl COLOC_input/" + pheno_name + "_eQTL_" + tissue + ".txt.gz -eqtl_sample_size " + tissue_sample_size + " -output COLOC_results/" + pheno_name + "_" + tissue + ".txt.gz" #COLOC prints out too much so throw it into a file
         os.system(run_COLOC + " > /dev/null 2>&1") #this script is a noisy boi
         print("Completed running COLOC in " + tissue + " for " + pheno_name + ".")
 print("Completed running COLOC. Have a good day :).")
