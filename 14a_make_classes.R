f60e4e6260d07d19e2e60719b70171ac38c045a2
Angela Andaleon <angie.andaleon@gmail.com>
2019-02-06 13:21:52 -0600

    remove test_pop

--- 14a_make_classes.R
+++ 14a_make_classes.R
@@ -6,7 +6,7 @@ library(data.table)
 args <- commandArgs(trailingOnly = T)
 phind_file_name <- args[1] #name of .phind file that HAPI-UR output
 pop_file_name <- args[2] #name of pop file with three columns: FID, IID, and pop
-test_pop <- args[3] #code of population to be tested
+#test_pop <- args[3] #code of population to be tested
 
 #parse .phind file
 phind <- fread(phind_file_name, header = F)
@@ -20,7 +20,7 @@ phind$V1 <- NULL
 pop_file <- fread("/home/angela/Ad_PX_pipe_data/RFMix/RefPop/" %&% pop_file_name %&% "_pop.txt", header = F)
 colnames(pop_file) <- c("FID", "IID", "pop")
 ordered_phind <- left_join(phind, pop_file, by = c("FID", "IID"))
-ordered_phind$pop <- as.numeric(factor(ordered_phind$pop, exclude = test_pop)) #convert pops to ints
+ordered_phind$pop <- as.numeric(factor(ordered_phind$pop)) #convert pops to ints
 ordered_phind$pop[ordered_phind$pop == max(ordered_phind$pop)] <- "0" #assign 0 to test pop (which should be the max if ordered properly)
-write(paste(as.character(ordered_phind$pop), collapse = " "), test_pop %&% ".classes")
+write(paste(as.character(ordered_phind$pop), collapse = " "), "RFMix.classes")
 
